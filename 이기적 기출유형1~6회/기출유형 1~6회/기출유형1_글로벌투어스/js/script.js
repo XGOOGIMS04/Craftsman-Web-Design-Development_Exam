0f2535b8431a76aaa310ff1f3a080263d6381a87
XGOOGIMS04 <heeju2021.front.end@gmail.com>
2026-07-05 21:14:06 +0900

    study: 기출유형1 헤더, 메뉴

--- 이기적 기출유형1~6회/기출유형 1~6회/기출유형1_글로벌투어스/js/script.js
+++ 이기적 기출유형1~6회/기출유형 1~6회/기출유형1_글로벌투어스/js/script.js
@@ -1,2 +1,9 @@
 $(function(){
+    // 메뉴
+    $("nav > ul > li").mouseover(function(){
+        $(this).children(".sub").stop().slideDown();
+    })
+    $("nav > ul > li").mouseout(function () {
+        $(".sub").stop().slideUp();
+    });
 })
