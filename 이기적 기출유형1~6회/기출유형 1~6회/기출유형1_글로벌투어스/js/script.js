c58d47d3d4dcf85cd83356f714a5a96af41817f7
XGOOGIMS04 <heeju2021.front.end@gmail.com>
2026-07-06 15:51:13 +0900

    기출유형1 slide, contents

--- 이기적 기출유형1~6회/기출유형 1~6회/기출유형1_글로벌투어스/js/script.js
+++ 이기적 기출유형1~6회/기출유형 1~6회/기출유형1_글로벌투어스/js/script.js
@@ -5,5 +5,27 @@ $(function(){
     })
     $("nav > ul > li").mouseout(function () {
         $(".sub").stop().slideUp();
-    });
+    })
+
+    // 슬라이드
+    let i = 0;
+    function slide(){
+        if(i < 2){  // 계속 증감하는것을 방지, 세번째 다음 첫번째가 보이도옥
+            i++;
+        }
+        else {
+            i = 0;
+        }
+        $(".slide ul").animate({ marginLeft: -1200 * i }, 1000); // 옆으로 이동
+    }
+    setInterval(slide, 3000); // 반복적 호출
+
+    // 팝업
+    $(".pop").click(function(){
+        $(".popup").show();
+        return false;
+    })
+    $(".close button").click(function(){
+        $(".popup").hide();
+    })
 })
