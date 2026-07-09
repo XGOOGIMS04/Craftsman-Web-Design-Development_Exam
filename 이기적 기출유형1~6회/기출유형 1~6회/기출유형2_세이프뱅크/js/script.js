419b548e6e71b312bd0ce188c05d02230edbfef5
XGOOGIMS04 <heeju2021.front.end@gmail.com>
2026-07-09 11:38:59 +0900

    study: 기출유형2 header, nav, slide 작업

---
+++ 이기적 기출유형1~6회/기출유형 1~6회/기출유형2_세이프뱅크/js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+    // 메뉴
+    $("nav > ul > li").mouseover(function(){
+        $(".sub, .menuBg").stop().slideDown();
+    })
+    $("nav > ul > li").mouseout(function(){
+        $(".sub, .menuBg").stop().slideUp();
+    })
+
+    // 슬라이드
+    let i = 0;
+    function slide(){
+        if(i < 2) {
+            i++;
+        }
+        else {
+            i = 0;
+        }
+        $(".slide ul li").fadeOut();
+        $(".slide ul li").eq(i).fadeIn();
+    }
+    setInterval(slide, 3000);
+})
