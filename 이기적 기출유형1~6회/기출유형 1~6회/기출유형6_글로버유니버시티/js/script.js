1c187d93f06268b1b575e4b141a051dfd62c1bab
XGOOGIMS04 <heeju2021.front.end@gmail.com>
2026-07-17 15:16:09 +0900

    study: 기출유형6 완료

--- 이기적 기출유형1~6회/기출유형 1~6회/기출유형6_글로버유니버시티/js/script.js
+++ 이기적 기출유형1~6회/기출유형 1~6회/기출유형6_글로버유니버시티/js/script.js
@@ -1,2 +1,31 @@
 $(function(){
+    // 메뉴
+    $("nav > ul > li").mouseenter(function(){
+        $(".sub").stop().slideDown();
+    })
+    $("nav > ul > li").mouseleave(function(){
+        $(".sub").stop().slideUp();
+    })
+
+    // 슬라이드
+    let i = 0;
+    function slide(){
+        if(i < 2){
+            i++;
+        } else {
+            i = 0;
+        }
+        $(".slide ul li").fadeOut();
+        $(".slide ul li").eq(i).fadeIn();
+    }
+    setInterval(slide, 3000);
+
+    //팝업
+    $(".pop").click(function(){
+        $(".popup").show();
+        return false;
+    })
+    $(".close button").click(function(){
+        $(".popup").hide();
+    })
 })
